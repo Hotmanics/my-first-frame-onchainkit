0e9678c91eedc71ec4eef010ab1b8eedc727b3b2
Jacob Homanics <homanicsjake@gmail.com>
2024-02-24 20:20:47 -0600

    yes

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -8,10 +8,21 @@ export const runtime = 'edge';
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
 
-    const nftName = searchParams.get('nftName');
-    const nftDescription = searchParams.get('nftDescription');
-    const nftImage = searchParams.get('nftImage');
+    const nftName0 = searchParams.get('nftName0');
+    const nftDescription0 = searchParams.get('nftDescription0');
+    const nftImage0 = searchParams.get('nftImage0');
 
+    const nftName1 = searchParams.get('nftName1');
+    const nftDescription1 = searchParams.get('nftDescription1');
+    const nftImage1 = searchParams.get('nftImage1');
+
+    const nftName2 = searchParams.get('nftName2');
+    const nftDescription2 = searchParams.get('nftDescription2');
+    const nftImage2 = searchParams.get('nftImage2');
+
+    const nftName3 = searchParams.get('nftName3');
+    const nftDescription3 = searchParams.get('nftDescription3');
+    const nftImage3 = searchParams.get('nftImage3');
     
   return new ImageResponse(
     (
@@ -19,31 +30,74 @@ export async function GET(request: Request) {
               background: '#F0944D',
               width: '100%',
               height: '100%',
-              padding: '50px 200px',
-              textAlign: 'center',
-              justifyContent: 'center',
-              alignItems: 'center',
             display: 'flex',
-            flexDirection: 'column'
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignContent: 'center'
             }}>
         
-        <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', background: 'grey'}}>
-            <div style={{fontSize: 18}}>
-            {nftName}
+        <div style={{display:'flex', height: '50%', width: '100%', alignContent: 'center', justifyContent: 'center'}}>
+
+        <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', background: 'grey', width: '20%', height: '100%', margin: 5}}>
+            <div style={{fontSize: 18, margin: 5}}>
+            {nftName0}
             </div>
             
-            <img src={nftImage!} width={106} height={106}/>            
+            <img src={nftImage0!} width={106} height={106} style={{margin: 5}}/>            
 
-            <div style={{fontSize: 11}}>
-            {nftDescription}
+            <div style={{margin: 5, fontSize: 11}}>
+            {nftDescription0}
             </div>
         </div>
+
+        <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', background: 'grey', width: '20%', height: '100%', margin: 5}}>
+            <div style={{fontSize: 18, margin: 5}}>
+            {nftName1}
+            </div>
+            
+            <img src={nftImage1!} width={106} height={106} style={{margin: 5}}/>            
+
+            <div style={{margin: 5, fontSize: 11}}>
+            {nftDescription1}
+            </div>
+        </div>
+
+        <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', background: 'grey', width: '20%', height: '100%', margin: 5}}>
+            <div style={{fontSize: 18, margin: 5}}>
+            {nftName2}
+            </div>
+            
+            <img src={nftImage2!} width={106} height={106} style={{margin: 5}}/>            
+
+            <div style={{margin: 5, fontSize: 11}}>
+            {nftDescription2}
+            </div>
+        </div>
+
+
+        <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', background: 'grey', width: '20%', height: '100%', margin: 5}}>
+            <div style={{fontSize: 18, margin: 5}}>
+            {nftName3}
+            </div>
+            
+            <img src={nftImage3!} width={106} height={106} style={{margin: 5}}/>            
+
+            <div style={{margin: 5, fontSize: 11}}>
+            {nftDescription3}
+            </div>
+        </div>
+
+        </div>
+        
+        <p style={{textAlign: 'center', justifyContent: 'center',
+            alignContent: 'center', fontSize: 32}}>*Actual NFTs fetched from Base Sepolia!</p>
+
 
     </div>
     ),
     {
-      width: 600,
-      height: 630,
+      width: 1200,
+      height: 780,
     },
   );
 }
